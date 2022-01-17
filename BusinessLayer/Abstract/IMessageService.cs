e97d080ae8a73657cbb1dd3e3e40fc5cb012f2f6
Mustafa EKİNCİ <mustafa.ekinci.93@gmail.com>
2022-01-17 17:24:34 +0300

    62. ders inbox hata fırlatıyor. Sonlarda kaldım.

---
+++ BusinessLayer/Abstract/IMessageService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IMessageService
+    {
+        List<Message> GetList();
+        void MessageAddBL(Message message);
+        Category GetByID(int id);
+        void MessageDeleteBL(Message message);
+        void MessageUpdateBL(Message message);
+    }
+}
